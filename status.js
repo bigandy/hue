3bbe2964b289efae8d4aec1cb2f7aa2531805b0a
Andrew Hudson <ahudson@gmail.com>
2016-11-28 17:54:59 +0000

    update api to 2.2.0

--- status.js
+++ status.js
@@ -1,45 +1,14 @@
-var config = require('./config.json');
-var HueApi = require("node-hue-api").HueApi;
+const config = require('./config.json');
+const HueApi = require("node-hue-api").HueApi;
 
-var displayResult = function(result) {
-	var lightOne = result.lights[1];
-	var lightTwo = result.lights[2];
-
-	// Visit non-inherited enumerable keys
-	Object.keys(result).forEach(function(key) {
-	    console.log(key, result[key]);
-	});
-
-
-
-	// console.log('Light One is ' + JSON.stringify(lightOne, null, 2) + ' level');
-	// console.log('Light Two is ' + JSON.stringify(lightTwo, null, 2) + ' level');
-
-	// console.log(lights);
-
-    // console.log(JSON.stringify(result.lights[1].state.bri, null, 2));
+const displayResult = function(result) {
+    console.log(JSON.stringify(result, null, 2));
 };
 
-var hostname = config.ip,
-    username = config.username,
-    api;
-
-api = new HueApi(hostname, username);
+const api = new HueApi(config.ip, config.username);
 
 // --------------------------
 // Using a promise
-api.getFullState().then(displayResult).done();
-// or alias fullState()
-// api.fullState().then(displayResult).done();
-
-// --------------------------
-// Using a callback
-// api.getFullState(function(err, config) {
-//     if (err) throw err;
-//     displayResult(config);
-// });
-// // or alias fullState()
-// api.fullState(function(err, config) {
-//     if (err) throw err;
-//     displayResult(config);
-// });
+api.lights()
+    .then(displayResult)
+    .done();
