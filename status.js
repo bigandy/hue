bf7cd9ef9c526c48d94d4e00c82e435cf60668ee
Andrew Hudson <ahudson@gmail.com>
2016-12-01 17:37:36 +0000

    turn light 4 on

--- status.js
+++ status.js
@@ -1,14 +1,33 @@
 const config = require('./config.json');
-const HueApi = require("node-hue-api").HueApi;
+const hue = require("node-hue-api");
+const HueApi = hue.HueApi;
+const lightState = hue.lightState;
 
 const displayResult = function(result) {
-    console.log(JSON.stringify(result, null, 2));
+	console.log(JSON.stringify(result, null, 2));
+
+	// for(let key in result.lights) {
+	// 	const jsonResult = JSON.stringify(result.lights[key], null, 2);
+	// 	console.log(jsonResult);
+	// 	// if (jsonResult.state.reachable) {
+	// 		// console.log(jsonResult);
+	// 	// }
+	// }
 };
+
+const state = lightState.create();
 
 const api = new HueApi(config.ip, config.username);
 
-// --------------------------
-// Using a promise
-api.lights()
-    .then(displayResult)
-    .done();
+api.setLightState(4, state.on())
+	.then(displayResult)
+	.done();
+
+// let a = 0;
+// setInterval(function () {
+// 	if (a % 61 === 0) {
+// 		a = 0;
+// 	}
+// 	console.log(a);
+// 	a++;
+// }, 100);
