30555266d02c69bdc5eb62cef17b8c39a1d27dce
Andrew Hudson <andrew@ventureharbour.com>
2019-06-27 15:11:26 +0100

    adds config-sample.json

--- register.js
+++ register.js
@@ -1,14 +1,14 @@
 var HueApi = require("node-hue-api").HueApi;
-
-var host = "192.168.1.24",
-    userDescription = "node";
+var config = require("./config.json");
+var host = config.ip,
+	userDescription = "node";
 
 var displayUserResult = function(result) {
-    console.log("Created user: " + JSON.stringify(result));
+	console.log("Created user: " + JSON.stringify(result));
 };
 
 var displayError = function(err) {
-    console.log(err);
+	console.log(err);
 };
 
 var hue = new HueApi();
@@ -16,13 +16,13 @@ var hue = new HueApi();
 // --------------------------
 // Using a promise
 hue.registerUser(host, userDescription)
-    .then(displayUserResult)
-    .fail(displayError)
-    .done();
+	.then(displayUserResult)
+	.fail(displayError)
+	.done();
 
 // --------------------------
 // Using a callback (with default description and auto generated username)
 hue.createUser(host, function(err, user) {
-    if (err) throw err;
-    displayUserResult(user);
+	if (err) throw err;
+	displayUserResult(user);
 });
